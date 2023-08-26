2e7bd6db219e8286673d9725b38889a0b0769527
Jack Davine <jackdavine47@gmail.com>
2023-08-26 19:53:22 +0800

    add temporary firrtl options

---
+++ src/main/scala/Generator.scala
@@ -0,0 +1,36 @@
+package risoco
+
+import chisel3.RawModule
+import firrtl.options.{Dependency, PhaseManager, Shell}
+import chisel3.stage.{ChiselCli, ChiselGeneratorAnnotation, ChiselStage}
+import firrtl.AnnotationSeq
+import firrtl.stage.FirrtlCli
+
+trait RisocoCli { this: Shell =>
+  parser.note("Risoco Cli notes")
+}
+
+class RisocoBaseStage extends ChiselStage {
+  override val shell = new Shell("risoco-chip") with RisocoCli with ChiselCli with FirrtlCli
+}
+
+abstract class FirrtlCompiler
+case object ScalaFC extends FirrtlCompiler
+
+object Generator {
+  def execute(args: Array[String], mod: => RawModule, fc: FirrtlCompiler, firtoolOpts: Array[String]) = {
+    fc match {
+      case ScalaFC =>
+        (new RisocoBaseStage).execute(args, Seq(
+          ChiselGeneratorAnnotation(mod _)
+        ))
+    }
+
+  }
+}
+
+object TestMain extends App {
+  var firOpts = Array[String]("-td", "build")
+  println("Begin generate hardwares.....")
+  Generator.execute(firOpts, new Hello, ScalaFC, Array.empty)
+}
